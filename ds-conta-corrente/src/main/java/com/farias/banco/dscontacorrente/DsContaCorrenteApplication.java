c30076cf63f5e45b04f583b55c560b68631d4cf9
isacfarias <isac.farias.21@gmail.com>
2021-03-15 14:11:02 -0400

    configure contta corrente client

---
+++ ds-conta-corrente/src/main/java/com/farias/banco/dscontacorrente/DsContaCorrenteApplication.java
@@ -0,0 +1,13 @@
+package com.farias.banco.dscontacorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsContaCorrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsContaCorrenteApplication.class, args);
+	}
+
+}
