63ff4c5f7d9aec90d2c4ade0a7e1a867a5925744
isacfarias <isac.farias.21@gmail.com>
2021-03-17 08:40:54 -0400

    adicionado teste para validar codigos de retorno dos endpoint, produtos e produtos faixa

--- ds-produtos/src/test/java/com/farias/banco/dsprodutos/ProdutosFaixaResourceTest.java
+++ ds-produtos/src/test/java/com/farias/banco/dsprodutos/ProdutosFaixaResourceTest.java
@@ -1,22 +1,32 @@
 package com.farias.banco.dsprodutos;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.farias.banco.dsprodutos.model.ProdutoFaixa;
+import com.farias.banco.dsprodutos.model.ProdutoTipo;
+import com.farias.banco.dsprodutos.repository.ProdutosFaixaRepository;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -28,6 +38,10 @@ public class ProdutosFaixaResourceTest {
 	@LocalServerPort
 	private int port;
 	
+	@MockBean
+	private ProdutosFaixaRepository repository;
+	
+	
 	private MockMvc mockMvc;
 
 	@BeforeEach
@@ -37,12 +51,28 @@ public class ProdutosFaixaResourceTest {
 
 	@Test
 	void deveRestonaStatus200_quandoEncontrarResultado() throws Exception {
+
+		when(repository.findAll()).thenReturn(Arrays.asList(new ProdutoFaixa(1, new ProdutoTipo(1, "Cheque especial"), 2, 5, new BigDecimal(1000.0)),
+				                                            new ProdutoFaixa(2, new ProdutoTipo(2, "Cartão crédito"), 2, 5, new BigDecimal(200.0))));
+		
 		this.mockMvc.perform(get("/produtosfaixa")
                     .contentType(MediaType.APPLICATION_JSON))
 		            .andDo(print())
 		            .andExpect(status().isOk())
 		            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 		            .andExpect(content().string(containsString("id")));
+	}
+	
+	
+	
+	@Test
+	void deveRestonaStatus404_quandoNaoEncontrarResultado() throws Exception {
+		when(repository.findAll()).thenReturn(new ArrayList<>());
+		
+		this.mockMvc.perform(get("/produtosfaixa")
+                    .contentType(MediaType.APPLICATION_JSON))
+		            .andDo(print())
+		            .andExpect(status().isNotFound());
 	}
 }	
 
--- ds-produtos/src/test/java/com/farias/banco/dsprodutos/ProdutosResourceTest.java
+++ ds-produtos/src/test/java/com/farias/banco/dsprodutos/ProdutosResourceTest.java
@@ -1,22 +1,30 @@
 package com.farias.banco.dsprodutos;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.farias.banco.dsprodutos.model.ProdutoTipo;
+import com.farias.banco.dsprodutos.repository.ProdutosTipoRepository;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -28,6 +36,9 @@ public class ProdutosResourceTest {
 	@LocalServerPort
 	private int port;
 	
+	@MockBean
+	private ProdutosTipoRepository repository;
+	
 	private MockMvc mockMvc;
 
 	@BeforeEach
@@ -37,12 +48,24 @@ public class ProdutosResourceTest {
 
 	@Test
 	void deveRestonaStatus200_quandoEncontrarResultado() throws Exception {
+		when(repository.findAll()).thenReturn(Arrays.asList(new ProdutoTipo(1, "Cheque especial"), new ProdutoTipo(2, "Cartão crédito")));
+		
 		this.mockMvc.perform(get("/produtos")
                     .contentType(MediaType.APPLICATION_JSON))
 		            .andDo(print())
 		            .andExpect(status().isOk())
 		            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 		            .andExpect(content().string(containsString("id")));
+	}
+	
+	@Test
+	void deveRestonaStatus404_quandoNaoEncontrarResultado() throws Exception {
+		when(repository.findAll()).thenReturn(new ArrayList<>());
+		
+		this.mockMvc.perform(get("/produtos")
+                    .contentType(MediaType.APPLICATION_JSON))
+		            .andDo(print())
+		            .andExpect(status().isNotFound());
 	}
 }	
 
